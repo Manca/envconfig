84b575e1c8c96b9ba2976393028f9759525b7dbc
Manca <manca1@gmail.com>
2013-11-10 17:00:21 +0100

    Setup.sh edited to separate installation from configuration

--- setup.sh
+++ setup.sh
@@ -4,45 +4,52 @@
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
-sudo apt-get install -y git
-sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# install
+if [ "$1" == 'install' ]; then
+    echo "Installing required packages..."
+    sudo apt-get install -y git
+    sudo apt-get install -y curl
+    curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
+    # Load nvm and install latest production node
+    source $HOME/.nvm/nvm.sh
+    nvm install v0.10.12
+    nvm use v0.10.12
 
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
+    # Install jshint to allow checking of JS code within emacs
+    # http://jshint.com/
+    npm install -g jshint
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+    # Install rlwrap to provide libreadline features with node
+    # See: http://nodejs.org/api/repl.html#repl_repl
+    sudo apt-get install -y rlwrap
 
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+    # Install emacs24
+    # https://launchpad.net/~cassou/+archive/emacs
+    sudo add-apt-repository -y ppa:cassou/emacs
+    sudo apt-get -qq update
+    sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+    # Install Heroku toolbelt
+    # https://toolbelt.heroku.com/debian
+    wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+fi
 
 # git pull and install dotfiles as well
+echo "Setting up environment..."
 cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
+if [ -d ./envconfig/ ]; then
+    mv envconfig  envconfig.old
 fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
 git clone https://github.com/Manca/envconfig.git
-ln -sb envconfig/.screenrc .
-ln -sb envconfig/.bash_profile .
-ln -sb envconfig/.bashrc .
-ln -sb envconfig/.bashrc_custom .
+ln -sf envconfig/.screenrc .
+ln -sf envconfig/.bash_profile .
+ln -sf envconfig/.bashrc .
+ln -sf envconfig/.bashrc_custom .
 ln -sf envconfig/.emacs.d .
-ln -sb envconfig/.gitconfig .
+ln -sf envconfig/.gitconfig .
+echo "Done."
